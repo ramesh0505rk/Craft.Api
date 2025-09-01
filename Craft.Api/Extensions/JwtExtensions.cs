78a7c8a00aac472b84ffc1e081a23f27a76a3ca0
Rameshkumar Krishnasamy <ramesh0505rk@gmail.com>
2025-09-01 10:31:17 +0530

    Initiated JwtExtensions.

---
+++ Craft.Api/Extensions/JwtExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Craft.Api.Extensions
+{
+    public static class JwtExtensions
+    {
+        public static IServiceCollection AddAuthentication(this IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateActor = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+        }
+    }
+}
